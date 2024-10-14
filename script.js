201d4abc7e1f2b0c93bab3bb43ffd9e7d30fe828
Rahul Sharma <96562610+imrawhul@users.noreply.github.com>
2024-10-14 16:25:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function clr() {
-  document.getElementById("lot").value = '';
+
+
   document.getElementById("qty").value = '';
   document.getElementById("bPrice").value = '';
   document.getElementById("sPrice").value = '';
@@ -11,7 +12,7 @@ function clr() {
 
 function calculate() {
   document.getElementById("outputs").style.display = "block";
-  var lot = document.getElementById("lot").value;
+  var lot = document.getElementById("index").value;
   var qty = document.getElementById("qty").value;
   var bPrice = document.getElementById("bPrice").value;
   var sPrice = document.getElementById("sPrice").value;
@@ -39,12 +40,12 @@ function calculate() {
     if (prft > 0) {
       outputs.style.border = "1px solid #1fa81f";
       outputs.style.backgroundColor = "rgba(0, 242, 95, 0.15)";
-      
+
     }
-    else{
+    else {
       outputs.style.border = "1px solid #ee2840";
       outputs.style.backgroundColor = "rgba(255, 61, 95, 0.15)";
-      }
+    }
 
   }
 
